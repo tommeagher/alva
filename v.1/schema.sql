e1a244c754bd88ce323b4fb9645f9fc2c91d47bc
tommeagher <car+bit@tommeagher.com>
2012-06-05 14:34:39 -0400

    Added substantial work on Flask version of app

---
+++ v.1/schema.sql
@@ -0,0 +1,10 @@
+drop table if exists entries;
+create table entries (
+  id integer primary key autoincrement,
+  title string not null,
+  text string not null,
+  publishdate date not null,
+  status not null,
+  notes not null,
+  private boolean DEFAULT True
+);
